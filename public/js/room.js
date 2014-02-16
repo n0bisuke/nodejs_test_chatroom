89c4a69ab8985c9e6a917edaa8b286033a9d3d8c
sugawara ryousuke <rsksound@gmail.com>
2014-02-16 23:31:25 +0900

    first commit

--- public/js/room.js
+++ public/js/room.js
@@ -1,40 +1,32 @@
-// room.js
-
-(function () {
+//room.js
+(function (){
   var socket;
   var messageLogs = {};
-
   // ページロード時の処理
-  $(document).ready(function () {
+  $(document).ready(function(){
     // ユーザー名、ルーム名、パスワードを送信
     socket = io.connect('http://localhost');
-
     // メッセージハンドラの定義
     // サーバーへの接続完了
-    socket.on('connected', function(data) {
+    socket.on('connected', function(data){
       socket.emit('check credential', minichat);
     });
-
     // 認証成功
     socket.on('credential ok', function(data) {
       socket.emit('request log', {});
     });
-
     // 認証失敗：ルーム名/パスワードの不一致
     socket.on('invalid credential', function(data) {
       authRetry('ルーム名/パスワードが不正です');
     });
-
     // 認証失敗：同名のルームがすでに存在
     socket.on('room exists', function(data) {
       authRetry('同名のルームがすでに存在します');
     });
-
     // 認証失敗：ルームに同名のユーザーが存在
     socket.on('userName exists', function(data) {
       authRetry('その名前はすでに使われています');
     });
-
     // チャットログの送信
     socket.on('request log', function(data, callback) {
       callback(messageLogs);
